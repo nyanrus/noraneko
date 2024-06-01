0655c794b73158650870c94608e684e87f067e35
Ryosuke Asano <ryosukeusuusu@gmail.com>
2024-06-01 11:05:03 +0900

    Refactoring with Class and fixing missing function

--- src/content/browser/statusbar/browser-statusbar.tsx
+++ src/content/browser/statusbar/browser-statusbar.tsx
@@ -11,17 +11,46 @@ export const [showStatusbar, setShowStatusbar] = createSignal(
 );
 
 createEffect(() => {
+  const statuspanel_label = document.getElementById(
+    "statuspanel-label",
+  ) as XULElement;
+  const statuspanel = document.getElementById("statuspanel") as XULElement;
+  const statusText = document.getElementById("status-text") as XULElement;
+
   Services.prefs.setBoolPref("browser.display.statusbar", showStatusbar());
-  const statuspanel_label = document.getElementById("statuspanel-label");
+
   if (showStatusbar()) {
     document.getElementById("status-text")?.appendChild(statuspanel_label!);
   } else {
     document.getElementById("statuspanel")?.appendChild(statuspanel_label!);
   }
+
+  const observer = new MutationObserver(() => {
+    if (statuspanel.getAttribute("inactive") === "true" && statusText) {
+      statusText.setAttribute("hidden", "true");
+    } else {
+      statusText?.removeAttribute("hidden");
+    }
+  });
+
+  observer?.disconnect();
+
+  if (showStatusbar()) {
+    observer.observe(statuspanel, { attributes: true });
+  }
 });
 
-export const gFloorpStatusBar = {
-  init() {
+class gFloorpStatusBarServices {
+  private static instance: gFloorpStatusBarServices;
+
+  public static getInstance() {
+    if (!gFloorpStatusBarServices.instance) {
+      gFloorpStatusBarServices.instance = new gFloorpStatusBarServices();
+    }
+    return gFloorpStatusBarServices.instance;
+  }
+
+  public init() {
     window.CustomizableUI.registerArea("statusBar", {
       type: window.CustomizableUI.TYPE_TOOLBAR,
       defaultPlacements: ["screenshot-button", "fullscreen-button"],
@@ -33,13 +62,15 @@ export const gFloorpStatusBar = {
     //move elem to bottom of window
     document.body?.appendChild(document.getElementById("statusBar")!);
     this.observeStatusbar();
-  },
+  }
 
-  observeStatusbar() {
+  private observeStatusbar() {
     Services.prefs.addObserver("browser.display.statusbar", () =>
       setShowStatusbar(() =>
         Services.prefs.getBoolPref("browser.display.statusbar", false),
       ),
     );
-  },
-};
+  }
+}
+
+export const gFloorpStatusBar = gFloorpStatusBarServices.getInstance();
