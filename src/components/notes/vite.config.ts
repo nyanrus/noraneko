6ac2a060134fb39e4c05a057edadd3313398adb4
Ryosuke Asano <s1F102400011@iniad.org>
2024-06-20 00:15:32 +0900

    Add "Notes" Sub Project

---
+++ src/components/notes/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  base: "./",
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+    reportCompressedSize: false,
+    minify: false,
+    rollupOptions: {
+      output: {
+        esModule: true,
+        entryFileNames: "[name].js",
+        assetFileNames: (chunk) => {
+          return `assets/${chunk.name ?? ""}`;
+        },
+        chunkFileNames: (chunk) => {
+          return `assets/${chunk.name}.js`;
+        },
+      },
+    },
+  },
+});
