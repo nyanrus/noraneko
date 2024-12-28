9ee3e7c2b53749a0121efd0c9870c64ecab111b9
NyanRus <68762426+nyanrus@users.noreply.github.com>
2024-12-28 21:27:36 +0900

    wip

--- build.ts
+++ build.ts
@@ -28,9 +28,11 @@ const isExists = async (path: string) => {
     .catch(() => false);
 };
 
-const getBinArchive = () => {
+const getBinArchive = async () => {
   if (process.platform === "win32") {
-    return "noraneko-win-amd64-dev.zip";
+    for await (const x of fs.glob("noraneko-*.en-US.win64.zip")) {
+      return x
+    }
   } if (process.platform === "linux") {
     const arch = process.arch;
     if (arch === "arm64") {
@@ -42,7 +44,7 @@ const getBinArchive = () => {
   throw new Error("Unsupported platform/architecture");
 };
 
-const binArchive = getBinArchive();
+const binArchive = await getBinArchive();
 const binDir = "_dist/bin";
 
 try {
